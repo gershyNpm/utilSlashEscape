72492aa43a05b69775e278c27968484c47afff74
Gershom Maes <gershommaes@protonmail.com>
2026-05-14 22:57:25 -0400

    <automated>

--- src/main.ts
+++ src/main.ts
@@ -1,14 +1,5 @@
 import '@gershy/clearing';
 
-// TODO: HEEERE2:
-// - ctrl+shift+f for:
-//    | 
-//    | "git[.]pub":
-//    | 
-// - Surfaces all files that haven't been updated with manager's template
-// - Clean em all up
-// - Make lilacLambda *happen*
-
 export default (str: string, escapeChars: string) => {
   if (!escapeChars[cl.has]('\\')) escapeChars += '\\';
   
